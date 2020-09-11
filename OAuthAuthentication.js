18679640dab728631dd484a7730852ff0dc100a4
Brenton House <brenton.house@gmail.com>
2020-09-11 15:56:34 -0500

    :thumbsup: v0.0.32 :heavy_minus_sign: Bumping version for project

--- OAuthAuthentication.js
+++ OAuthAuthentication.js
@@ -11,13 +11,13 @@ class OAuthAuthentication {
 			case 'code':
 				// TIBUG:  https://jira.appcelerator.org/browse/TIMOB-28037
 				// Flow = require('./flows/CodeFlow');
-				Flow = require(`${__dirname}/flows/Code`);
+				Flow = require(`./flows/Code`);
 				break;
 
 			case 'password':
 				// TIBUG:  https://jira.appcelerator.org/browse/TIMOB-28037
 				// Flow = require('./flows/OwnerResourceFlow');
-				Flow = require(`${__dirname}/flows/Password`);
+				Flow = require(`./flows/Password`);
 				break;
 
 			default:
